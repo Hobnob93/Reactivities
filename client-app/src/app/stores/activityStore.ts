9a3bd9828758b659caf2b6c5b2bc779d2982994c
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2020-03-05 10:17:19 +0000

    using async await instead of then-catch-finally

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -9,15 +9,20 @@ class ActivityStore {
     @observable loadingInitial = false;
     @observable editMode = false;
 
-    @action loadActivities = () => {
+    @action loadActivities = async () => {
         this.loadingInitial = true;
-        agent.Activities.list()
-            .then(activities => {
-                activities.forEach(a => {
-                    a.date = a.date.split(".")[0];
-                    this.activities.push(a);
-                });
-            }).finally(() => this.loadingInitial = false);
+        try {
+            const activities = await agent.Activities.list();
+
+            activities.forEach(a => {
+                a.date = a.date.split(".")[0];
+                this.activities.push(a);
+            });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            this.loadingInitial = false
+        }
     };
 
     @action selectActivity = (id: string) => {
