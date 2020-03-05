7a9fc7196c92056397e66f849ab90d5695a0a944
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2020-03-05 13:49:15 +0000

    Nav links now working

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../app/stores/activityStore';
+import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
     const activityStore = useContext(ActivityStore);
@@ -9,13 +10,13 @@ const NavBar: React.FC = () => {
     return (
         <Menu fixed="top" inverted>
             <Container>
-                <Menu.Item header>
+                <Menu.Item header as={NavLink} exact to="/">
                     <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}} />
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name='Activities' />
+                <Menu.Item name='Activities' as={NavLink} to="/activities" />
                 <Menu.Item>
-                    <Button positive content="Create Activity" onClick={() => activityStore.openCreateForm()} />
+                    <Button positive as={NavLink} to="/createActivity" content="Create Activity" />
                 </Menu.Item>
             </Container>
         </Menu>
