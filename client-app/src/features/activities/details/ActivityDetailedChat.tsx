14cc45fb02295ca0a3578b3df25eb1657e6dc7e0
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2020-03-05 17:10:08 +0000

    Activity chat section styled and with basic outline content

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Segment, Header, Comment, Form, Button } from 'semantic-ui-react'
 
 const ActivityDetailedChat = () => {
     return (
-        <div>
-            <h1>Chat</h1>
-        </div>
+        <Fragment>
+            <Segment textAlign="center" attached="top" inverted color="teal" style={{ border: "none" }}>
+                <Header>Chat about this event</Header>
+            </Segment>
+            <Segment attached>
+                <Comment.Group>
+                    <Comment>
+                        <Comment.Avatar src="/assets/user.png" />
+                        <Comment.Content>
+                            <Comment.Author as="a">Matt</Comment.Author>
+                            <Comment.Metadata>
+                                <div>Today at 17:42</div>
+                            </Comment.Metadata>
+                            <Comment.Text>How artistic!</Comment.Text>
+                            <Comment.Actions>
+                                <Comment.Action>Reply</Comment.Action>
+                            </Comment.Actions>
+                        </Comment.Content>
+                    </Comment>
+
+                    <Comment>
+                        <Comment.Avatar src="/assets/user.png" />
+                        <Comment.Content>
+                            <Comment.Author as="a">Joe Henerson</Comment.Author>
+                            <Comment.Metadata>
+                                <div>5 days ago</div>
+                            </Comment.Metadata>
+                            <Comment.Text>Dude, this is awesome!</Comment.Text>
+                            <Comment.Actions>
+                                <Comment.Action>Reply</Comment.Action>
+                            </Comment.Actions>
+                        </Comment.Content>
+                    </Comment>
+
+                    <Form reply>
+                        <Form.TextArea />
+                        <Button content="Add Reply" labelPosition="left" icon="edit" primary />
+                    </Form>
+                </Comment.Group>
+            </Segment>
+        </Fragment>
     )
 }
 
