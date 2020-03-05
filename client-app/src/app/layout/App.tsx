a6bfd19896b9774946046aea15f3cb06d5562608
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2020-03-05 13:45:04 +0000

    Home Page routing

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,6 +5,9 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import LoadingComponent from './LoadingComponent';
 import ActivityStore from '../stores/activityStore'
 import NavBar from '../../features/nav/NavBar';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 const App = () => {
   const activityStore = useContext(ActivityStore)
@@ -20,7 +23,9 @@ const App = () => {
       <NavBar />
       <Container style={{marginTop: '7em'}}>
         <List>
-          <ActivityDashboard />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/activities" component={ActivityDashboard} />
+          <Route path="/createActivity" component={ActivityForm} />
         </List>
       </Container>
     </Fragment>
