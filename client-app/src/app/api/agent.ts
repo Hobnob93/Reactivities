fc38b75f30a76fb836799241aafbaa755225f0e0
Kyle Hobdey <khobdey@cyberwolfstudios.co.uk>
2020-03-06 11:19:46 +0000

    Toast for 500 error

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';
 import { history } from '../..';
+import { toast } from 'react-toastify';
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
@@ -10,8 +11,13 @@ axios.interceptors.response.use(undefined, error => {
     if (status === 404) {
         history.push("/notfound");
     }
+
     if (status === 400 && config.method === "get" && data.errors.hasOwnProperty("id")) {
         history.push("/notfound");
+    }
+
+    if (status === 500) {
+        toast.error("Server error - check terminal for more info!");
     }
 });
 
